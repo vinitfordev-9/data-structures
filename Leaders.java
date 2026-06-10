3e7b3927e71bd6e9441eeff2b02be1fa29c349af
CBT-232 Vinit Sudhakar <vinitplacements9@gmail.com>
2026-06-10 08:36:58 +0530

    Leared and Implemneted the optimal solution for leaders problem

--- Leaders.java
+++ Leaders.java
@@ -1,32 +1,47 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 class Solution {
 
-    static void bruteApproach(int[] a, int n) {
+    static void optimalApproach(int[] a, int n) {
         ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            boolean leader = true;
 
-            for (int j = i + 1; j < n; j++) {
-                if (a[j] >= a[i]) {
-                    leader = false;
-                    break;
-                }
-            }
-
-           
-            if (leader) {
+        int max = a[n - 1];
+        list.add(max);
+        for (int i = n - 2; i >= 0; i--) {
+            if (a[i] > max) {
                 list.add(a[i]);
+                max = a[i];
             }
         }
+        Collections.reverse(list);
         System.out.println("The leaders are:");
-        for(int res:list){
-            System.out.println(" "+res);
+        for (int res : list) {
+            System.out.println(" " + res);
         }
     }
 
+    // static void bruteApproach(int[] a, int n) {
+    //     ArrayList<Integer> list = new ArrayList<>();
+    //     for (int i = 0; i < n; i++) {
+    //         boolean leader = true;
+    //         for (int j = i + 1; j < n; j++) {
+    //             if (a[j] >= a[i]) {
+    //                 leader = false;
+    //                 break;
+    //             }
+    //         }
+    //         if (leader) {
+    //             list.add(a[i]);
+    //         }
+    //     }
+    //     System.out.println("The leaders are:");
+    //     for(int res:list){
+    //         System.out.println(" "+res);
+    //     }
+    // }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the value of n");
@@ -37,6 +52,7 @@ class Solution {
 
             a[i] = sc.nextInt();
         }
-        bruteApproach(a, n);
+        // bruteApproach(a, n);
+        optimalApproach(a, n);
     }
 }
